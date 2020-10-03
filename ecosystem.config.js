13a67734cc5ce4ff19beebd1970a7e4a54738a1f
Daniel Koziara <daniel@impresspro.pl>
2020-10-03 18:07:29 +0200

    test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,23 @@
 module.exports = {
-  apps : [{
-    script: 'index.js',
-    watch: '.'
-  }],
+  apps: [
+    {
+      script: "index.js",
+      watch: ".",
+    },
+  ],
 
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
-    }
-  }
+  deploy: {
+    production: {
+      key: "/home/daniel/.ssh/id_rsa",
+      user: "pi",
+      host: ["192.168.0.19"],
+      ssh_options: "StrictHostKeyChecking=no",
+      ref: "origin/master",
+      repo: "git@github.com:danielkoziara/test-deploy-raspberry-pi.git",
+      path: "/home/pi/apps/test-api",
+      "pre-deploy-local": "",
+      "post-deploy": "npm install && pm2 reload ecosystem.config.js",
+      "pre-setup": "",
+    },
+  },
 };
--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-
-
-app.get('/',(req,res) => { 
-   const response = {date: new Date().toLocaleString()};
-   console.log(response);
-   return res.json(response);
+app.get("/", (req, res) => {
+  const response = {
+    date: new Date().toLocaleString(),
+    env: process.env.NODE_ENV,
+  };
+  console.log(response);
+  return res.json(response);
 });
 
-
-app.listen(8080, () => console.log('Server listen on 8080 port.'))
+app.listen(8080, () => console.log("Server listen on 8080 port."));
